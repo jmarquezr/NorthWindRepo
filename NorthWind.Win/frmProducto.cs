cda934a6fc6444cf1289d680c249405f387a694e
jmarquezr <jmarqzr@gmail.com>
2015-04-12 10:05:08 -0500

    Modificacion Agregacion de productos

--- NorthWind.Win/frmProducto.cs
+++ NorthWind.Win/frmProducto.cs
@@ -14,6 +14,7 @@ namespace NorthWind.Win
     
     public partial class frmProducto : Form
     {
+        public event EventHandler<TbProductoBE> onProductoSeleccionado;
         List<TbProductoBE> Lista = new List<TbProductoBE>();
         public frmProducto()
         {
@@ -27,6 +28,31 @@ namespace NorthWind.Win
             this.dataGridView1.SelectionMode =
                 DataGridViewSelectionMode.FullRowSelect;
 
+        }
+
+        private void AgregarProductoFactura()
+        {
+            int i = dataGridView1.CurrentRow.Index;
+            string codigoProducto = dataGridView1.Rows[i].Cells[0].Value.ToString();
+            TbProductoBE oProducto = (from item in Lista.ToArray()
+                                    where item.CodProducto == codigoProducto
+                                    select item).Single();
+            onProductoSeleccionado(new object(), oProducto);
+            this.Close();
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            AgregarProductoFactura();
+        }
+
+        private void dataGridView1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Return)
+            {
+                AgregarProductoFactura();
+            }
         }
     }
 }
